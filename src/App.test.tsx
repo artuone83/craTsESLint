b415a0f5010b754026fb89e5069a74eb10e339fe
Artur Wozniak <wozniaka83@gmail.com>
2021-11-06 13:54:28 +0100

    jest config and vsc tasks

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Name of the group', () => {
+  test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText('Test absolute path');
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('should alway be true', () => {
+    expect(true).toBe(true);
+  });
+
+  test('should alway be false', () => {
+    expect(false).toBe(false);
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Hello from 'components/Hello/Hello';
+import Hello from 'components/Hello';
 
 const App = () => (
   <div className="App">
